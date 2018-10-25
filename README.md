c8ed616b2c76557276b58b0d3b83a23631602e6b
“Christina <cfthompson321@gmail.com>
2018-10-25 15:44:17 -0400

    final

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Jeopardy-project1
+
+Description
+This game is a play on the infamous Jeopardy trivia game. Users are able to select a dollar amount and answer the corresponding question in the traditional " who is.." "what is.." format. lower case and gramatically correct spacing will be accepted. This game is for the solitarie-type player looking to test their progressive political knowledge or learn something new. The game is not timed, users are given the opportunity to google answers they do not know. This game introduces lesser known facts and prominent figures that are not always recognized, and/or highlights the hyprocrisy of our current legislative system.
+
+## Example
+
+Category selected with accompanying question.
+
+You're correct!
+
+Not quite, Try Again!
+
+## List of features/User Stories
+
+- Player can click on dollar amount in a category of their choice
+- Once clicked, the corresponding question will generate with accompanying lined text box
+- User will type in answer, click okay button to confirm answer, user can click cancel to exit the question
+- If user answers correctly, window prompt will confirm, user is redirected back to home page
+- If incorrect, window prompt will appear “Sorry, try again!”, user redirected to home page to select another question
+- User selects new question, repeat process
+
+## Tech Used
+
+- Javascript
+- HTML
+- CSS
--- script.js
+++ script.js
@@ -5,18 +5,21 @@ var jeopardy = {
     qOne:
       "This infamous quote 'Im sick and tired, of being sick and tired!' used by Black women when they've had enough, was coined by this civil right activist.",
     qTwo:
-      "This activist said 'Me Too' 12 years prior to the movements present day prominence, but she is often forgotten as its creator",
+      "This activist said 'Me Too' 12 years prior to the movements present day prominence, but she is often forgotten as its creator.",
     qThree:
-      "This activist climbed the Statue of Liberty in protest of the inhumane seperation of families at the US/Mexico border",
+      "This activist climbed the Statue of Liberty in protest of the inhumane seperation of families at the US/Mexico border.",
     qFour:
-      "The 1994 crime bill, the catalyst for the mass incarceration of people of color was authored by this former Delaware senator",
+      "The 1994 crime bill, the catalyst for the mass incarceration of people of color was authored by this former Delaware senator.",
     qFive:
-      "The Anti-Drug Abuse Act of 1986 established mandatory prison sentences for low level drug offenses, disportionally targeting communities of color, is known by what pseudoym? ",
+      "The Anti-Drug Abuse Act of 1986 established mandatory prison sentences for low level drug offenses, disportionally targeting communities of color, is known by what pseudoym?. ",
     qSix:
-      "This amendment ratified in 1865, reestablished slavery as form of punishment, generating roughly $3.9 billion dollars a year in free labor from inmates ",
-    qSeven: "",
-    qEight: "",
-    qNIne: ""
+      "This Amendment ratified in 1865, reestablished slavery as form of punishment, generating roughly $3.9 billion dollars a year in free prison labor from inmates. ",
+    qSeven:
+      "This Supreme Court Justice believed Black students do not excel at top-tier schools, and even suggested they go to 'slower-track' schools, continuing the age old trope that Black people are not intelligent.",
+    qEight:
+      "This Supreme Court Justice said 'society can prevent those who are manifestly unfit from continuing their kind' ruling individuals with intellectual disabilities should be permanetnly sterilized. ",
+    qNine:
+      "This 1896 Supreme Court decision upheld racial segregation with the ruling 'seperate but equal' launching the US into the Jim Crow era. "
   },
 
   answers: {
@@ -26,9 +29,9 @@ var jeopardy = {
     qFour: "who is joe biden",
     qFive: "what is the war on drugs",
     qSix: "what is the 13th amendment",
-    qSeven: "",
-    qEight: "",
-    qNine: ""
+    qSeven: "who is justice antonin scalia",
+    qEight: "who is justice oliver holmes",
+    qNine: "what is plessy v. ferguson"
   },
 
   checkAnswer: function(question) {
@@ -43,9 +46,8 @@ var jeopardy = {
       if (this.answers[question] === this.answer) {
         this.success = true;
         alert("YASSSS!");
-        //make question anchor inactive
         //close alert box
-        break; //break or return bc we are done
+        break;
       } else {
         this.attempts++;
         alert("That ain't it...try again!");
