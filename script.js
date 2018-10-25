8e208609ff2f983f2fd8c86fb2f45aa204417c92
“Christina <cfthompson321@gmail.com>
2018-10-25 13:12:35 -0400

    added questions,loop no longer gives 3 attempts

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ console.log("hi");
 var jeopardy = {
   questions: {
     qOne:
-      "This activist said 'Me Too' 12 years prior to the movements present day prominece, but she is often forgotten as its creator",
+      "This infamous quote 'Im sick and tired, of being sick and tired!' used by Black women when they've had enough, was coined by this civil right activist.",
     qTwo:
-      "This immigration activist climbed the Statue of Liberty in protest of the seperation of families at the US/Mexico border",
+      "This activist said 'Me Too' 12 years prior to the movements present day prominence, but she is often forgotten as its creator",
     qThree:
-      "The 1994 crime bill, the catalyst for the mass incarceration of people of color was authored by this former Delaware senator",
+      "This activist climbed the Statue of Liberty in protest of the inhumane seperation of families at the US/Mexico border",
     qFour:
-      "This civil rights herione said this famous quote in a speech at the 1964 Democratic National Convention 'Im sick and tired, of being sick and tired!'",
-    qFive: "",
-    qSix: ""
+      "The 1994 crime bill, the catalyst for the mass incarceration of people of color was authored by this former Delaware senator",
+    qFive:
+      "The Anti-Drug Abuse Act of 1986 established mandatory prison sentences for low level drug offenses, disportionally targeting communities of color, is known by what pseudoym? ",
+    qSix:
+      "This amendment ratified in 1865, reestablished slavery as form of punishment, generating roughly $3.9 billion dollars a year in free labor from inmates ",
+    qSeven: "",
+    qEight: "",
+    qNIne: ""
   },
 
   answers: {
-    qOne: "who is tarana burke",
-    qTwo: "who is therese okumou",
-    qThree: "who is joe biden",
-    qFour: "who is fannie louhamer",
-    qFive: "",
-    qSix: ""
+    qOne: "who is fannie louhamer",
+    qTwo: "who is tarana burke",
+    qThree: "who is therese okumou",
+    qFour: "who is joe biden",
+    qFive: "what is the war on drugs",
+    qSix: "what is the 13th amendment",
+    qSeven: "",
+    qEight: "",
+    qNine: ""
   },
 
   checkAnswer: function(question) {
@@ -34,20 +42,19 @@ var jeopardy = {
       );
       if (this.answers[question] === this.answer) {
         this.success = true;
-        alert("You are correct!");
+        alert("YASSSS!");
         //make question anchor inactive
         //close alert box
         break; //break or return bc we are done
       } else {
         this.attempts++;
-        alert("You are incorrect...try again");
+        alert("That ain't it...try again!");
         //return to text box
         break;
       }
     }
   }
 };
-// only works on intial load, need to be looped. have to refresh between selection
 const qOne = document.getElementsByClassName("box qOne");
 console.log(qOne);
 qOne[0].addEventListener("click", function() {
@@ -82,4 +89,22 @@ const qSix = document.getElementsByClassName("box qSix");
 console.log(qSix);
 qSix[0].addEventListener("click", function() {
   jeopardy.checkAnswer("qSix");
+});
+
+const qSeven = document.getElementsByClassName("box qSeven");
+console.log(qSeven);
+qSeven[0].addEventListener("click", function() {
+  jeopardy.checkAnswer("qSeven");
+});
+
+const qEight = document.getElementsByClassName("box qEight");
+console.log(qEight);
+qEight[0].addEventListener("click", function() {
+  jeopardy.checkAnswer("qEight");
+});
+
+const qNine = document.getElementsByClassName("box qNine");
+console.log(qNine);
+qNine[0].addEventListener("click", function() {
+  jeopardy.checkAnswer("qNine");
 });
